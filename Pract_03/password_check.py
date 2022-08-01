a4b27a833f37adc45f50f82e1a12189f5cbd2de8
Sai Kham <saikhamthuranyunt@my.jcu.edu.au>
2022-08-02 03:43:41 +0630

    "Add password check program"

--- Pract_03/password_check.py
+++ Pract_03/password_check.py
@@ -1,6 +1,18 @@
-MINIMUM_CHARACTERS= 10
-password = input("Enter password of at least 10 characters :")
-while len(password) < MINIMUM_CHARACTERS:
-    print("Password does not meet requirements")
+
+def main():
+    MINIMUM_CHARACTERS = 10
+    password = get_password(MINIMUM_CHARACTERS)
+    print_asterisk(password)
+
+def get_password(MINIMUM_CHARACTERS):
     password = input("Enter password of at least 10 characters :")
-print ("*" * len(password))
+    while len(password) < MINIMUM_CHARACTERS:
+        print("Password does not meet requirements")
+        password = input("Enter password of at least 10 characters :")
+    return password
+
+def print_asterisk(password):
+    print("*" * len(password))
+
+
+main()
