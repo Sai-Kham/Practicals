7ecbfc58af6e877e6883544264707820a6f3b6b0
Sai Kham <saikhamthuranyunt@my.jcu.edu.au>
2022-08-09 00:18:56 +0630

    "Lists warm up"

---
+++ Pract_04/lists_warmup.py
@@ -0,0 +1,16 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# first element of the number to "ten"
+# numbers[0] = "TEN"
+# print(numbers)
+
+#last element of the numbers to 1
+# numbers[6] = 1
+# print(numbers)
+
+# all elements from the numbers except the first two (Slice)
+# print(numbers[2:])
+
+#check if 9 is an element of numbers
+print(9 in numbers)
+
