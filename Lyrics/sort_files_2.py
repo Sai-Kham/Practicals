b7c9b3563bb2b6a620a606c266415be1c71bb5cb
Sai Kham <saikhamthuranyunt@my.jcu.edu.au>
2022-09-20 17:39:19 +0630

    "Sorting ver 2"

---
+++ Lyrics/sort_files_2.py
@@ -0,0 +1,24 @@
+import os
+
+
+def main():
+    extension_categories = {}
+    os.chdir('FilesToSort')
+    for filename in os.listdir('.'):
+        if os.path.isdir(filename):
+            continue
+        extension = filename.split('.')[-1]
+
+        if extension not in extension_categories:
+            category = input(f"What category would you like to sort {extension} files into? ")
+            extension_categories[extension] = category
+
+            try:
+                os.mkdir(category)
+            except FileExistsError:
+                pass
+
+        os.rename(filename, f"{extension_categories[extension]}/{filename}")
+
+    if __name__ == '__main__':
+        main()
